962c83955999b274609c741d98d4d7ffbe06beb7
Tarun-sahu-git <tarunsahut754@gmail.com.in>
2026-02-12 21:06:52 +0530

    add path for update and delete

--- backend/Router/user_routes.js
+++ backend/Router/user_routes.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const router=express.Router()
-const {fetchuserRecord,createuserRecord}=require('../Cotroller/user')
+const {fetchuserRecord,createuserRecord,deleteuserRecord,updateuserDAta}=require('../Cotroller/user')
 
 router.get('/',fetchuserRecord)
 router.post('/',createuserRecord)
+router.put('/:id',updateuserDAta)
+router.delete('/:id',deleteuserRecord)
+
 
 module.exports=router
